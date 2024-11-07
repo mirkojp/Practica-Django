715f727b863d4c4692658301a7616dd8e1734b21
mirkojp <mirkopavan88878@gmail.com>
2024-11-07 15:47:39 -0300

    Fallando en integracion MP  7 11 24

--- Compras/urls.py
+++ Compras/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('carritos', views.carritos, name="carritos"),
-    path('compras', views.compras, name="compras"),
-    path('compras/<int:id>', views.operaciones_compras, name="operaciones_compras"),
+    path("carritos", views.carritos, name="carritos"),
+    path("compras", views.compras, name="compras"),
+    path("compras/<int:id>", views.operaciones_compras, name="operaciones_compras"),
+    path("proceso-pago/", views.ProcesoPagoAPIView.as_view(), name="proceso_pago"),
 ]
