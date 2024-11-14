2ad71d8e4b1643a5174ba5af76b0c2d096f4d01b
mirkojp <mirkopavan88878@gmail.com>
2024-11-14 10:12:21 -0300

    kill me

--- Compras/urls.py
+++ Compras/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path("compras/<int:id>", views.operaciones_compras, name="operaciones_compras"),
     # path("proceso-pago/", views.ProcesoPagoAPIView.as_view(), name="proceso_pago"),
     path("create_preference/", views.CreatePreferenceView.as_view(), name="create_preference"),
-    path("get_token/",views.getToken,name="get_token")
 ]
