69a4a9a9f3479f5f4cd8babf767a438a697bd5f0
mirkojp <mirkopavan88878@gmail.com>
2025-03-12 01:25:23 -0300

    Modificar URLS
    *añadir .as_views()
    
    :D quiero dormir

--- Productos/urls.py
+++ Productos/urls.py
@@ -3,17 +3,33 @@ from . import views
 from .views import ImagenListView
 
 urlpatterns = [
-    path('funkos', views.funkos, name="funkos"),
-    path('funkos/<int:id>', views.operaciones_funkos, name="operaciones_funkos"),
-    path('funkos/<int:id>/favoritos', views.favoritos, name="favoritos"),
-    path('descuentos', views.descuentos, name="descuentos"),
-    path('descuentos/<int:id>', views.operaciones_descuentos, name="operaciones_descuentos"),
-    path('funkodescuentos', views.funkoDescuentos, name="funkoDescuentos"),
-    path('funkodescuentos/<int:id>', views.op_funkoDescuentos, name="op_funkoDescuentos"),
-    path('categorias', views.categorias, name="categorias"),
-    path('categorias/<int:id>', views.op_categorias, name="op_categorias"),
-    path("imagenes/", ImagenListView, name="lista-imagenes"),  # GET para todas las imágenes, POST para subir una
-    path("imagenes/<int:imagen_id>/", ImagenListView, name="detalle-imagen"),
-    path('categorias/<int:id>/gestionar', views.gestionar_funkos_categoria, name="gestionar_funkos_categoria"),
-    path('funkos/<int:id>/reseñas', views.listar_reseñas_funko, name="listar_reseñas_funko"),
+    path("funkos", views.funkos, name="funkos"),
+    path("funkos/<int:id>", views.operaciones_funkos, name="operaciones_funkos"),
+    path("funkos/<int:id>/favoritos", views.favoritos, name="favoritos"),
+    path("descuentos", views.descuentos, name="descuentos"),
+    path(
+        "descuentos/<int:id>",
+        views.operaciones_descuentos,
+        name="operaciones_descuentos",
+    ),
+    path("funkodescuentos", views.funkoDescuentos, name="funkoDescuentos"),
+    path(
+        "funkodescuentos/<int:id>", views.op_funkoDescuentos, name="op_funkoDescuentos"
+    ),
+    path("categorias", views.categorias, name="categorias"),
+    path("categorias/<int:id>", views.op_categorias, name="op_categorias"),
+    path(
+        "imagenes/", ImagenListView.as_view(), name="lista-imagenes"
+    ),  # GET para todas las imágenes, POST para subir una
+    path("imagenes/<int:imagen_id>/", ImagenListView.as_view(), name="detalle-imagen"),
+    path(
+        "categorias/<int:id>/gestionar",
+        views.gestionar_funkos_categoria,
+        name="gestionar_funkos_categoria",
+    ),
+    path(
+        "funkos/<int:id>/reseñas",
+        views.listar_reseñas_funko,
+        name="listar_reseñas_funko",
+    ),
 ]
